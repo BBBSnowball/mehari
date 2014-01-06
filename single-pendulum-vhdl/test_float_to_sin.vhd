00df22517ec108b0f8b6ab13d402b36c5de3bcf1
Benjamin Koch <bbbsnowball@gmail.com>
2014-01-06 20:00:55 +0100

    make timing completely synchronous for test_float_to_sin

--- single-pendulum-vhdl/test_float_to_sin.vhd
+++ single-pendulum-vhdl/test_float_to_sin.vhd
@@ -91,26 +91,23 @@ BEGIN
   end process;
  
 
-  -- Stimulus process
-  stim_proc: process
-    procedure test(input_value : in real) is
+  stimulus_process: process
+    procedure test(input_value : in real; max_clock_cycles : in integer := 100) is
     begin
-      wait until s_axis_a_tready = '1' for 10*aclk_period;
+      wait until s_axis_a_tready = '1' and rising_edge(aclk) for 10*aclk_period;
       assert s_axis_a_tready = '1' report "uut is not ready for data";
-      --wait for 0 ns;
 
+      wait until falling_edge(aclk);
       s_axis_a_tdata <= to_float(input_value);
       s_axis_a_tvalid <= '1';
 
-      wait for 2*aclk_period;
+      wait for aclk_period;
 
       s_axis_a_tvalid <= '0';
-      --wait for 0 ns;
       s_axis_a_tdata <= (others => '0');
 
-      wait until m_axis_result_tvalid = '1' and rising_edge(aclk) for 100*aclk_period;
+      wait until m_axis_result_tvalid = '1' and rising_edge(aclk) for max_clock_cycles*aclk_period - aclk_period/2;
       assert m_axis_result_tvalid = '1' report "result was not ready in time";
-      --wait for 0 ns;
 
       if m_axis_result_tvalid = '1' then
         assertEqual(m_axis_result_tdata, std_logic_vector(to_cordic_in(input_value)));
@@ -118,21 +115,18 @@ BEGIN
     end procedure;
 
     constant null_X_40 : std_logic_vector(39 downto 0) := (others => '0');
-  begin		
-      -- hold reset state for 100 ns.
+  begin
       wait for 100 ns;
 
       m_axis_result_tready <= '1';
 
       wait for aclk_period*10;
 
-      -- insert stimulus here
-
       assertEqual(std_logic_vector(to_cordic_in( 2.0)), "01000000" & null_X_40);
       assertEqual(std_logic_vector(to_cordic_in( 1.5)), "00110000" & null_X_40);
       assertEqual(std_logic_vector(to_cordic_in(-1.5)), "11010000" & null_X_40);
 
-      test(2.0);
+      test(2.0, 7);
       test(1.5);
       test(math_pi);
       test(-math_pi);
@@ -142,4 +136,21 @@ BEGIN
       wait;
   end process;
 
+  debug_timing_process : process(aclk)
+    variable input_was_valid  : std_logic := '0';
+    variable output_was_valid : std_logic := '0';
+  begin
+    if rising_edge(aclk) then
+      if s_axis_a_tvalid = '1' and input_was_valid = '0' then
+        report "data on input is valid";
+      end if;
+
+      if m_axis_result_tvalid = '1' and output_was_valid = '0' then
+        report "output data is valid";
+      end if;
+
+      input_was_valid := s_axis_a_tvalid;
+      output_was_valid := m_axis_result_tvalid;
+    end if;
+  end process;
 END;
