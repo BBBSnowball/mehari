f2937d0b331ca2b93f3ffd0b2c5a45011660036a
Benjamin Koch <bbbsnowball@gmail.com>
2014-01-15 03:19:01 +0100

    add DONT_TEST mode for test-ci-all.sh -> user can use it to prepare for manually running tests

--- test-ci-all.sh
+++ test-ci-all.sh
@@ -8,13 +8,18 @@ cd "$(dirname "$0")"
 # are available for all further scripts
 export ENVVAR_STORE="$(pwd)/.tmp-envvar_store"
 [ -e "$ENVVAR_STORE" ] && rm "$ENVVAR_STORE"
+ENVVAR_STORE_ALL=".tmp-envvar_store-all"
+[ -e "$ENVVAR_STORE_ALL" ] && rm "$ENVVAR_STORE_ALL"
 
 echo "Gathering task list..."
 
-find -name test-ci.sh -or -regex ".*/ci-[0-9]+-[^/ ]+\.[^/ ]+" | awk -F "/" '{print $NF " " $0}' \
+find -regex ".*/ci-[0-9]+-[^/ ]+\.[^/ ]+" | awk -F "/" '{print $NF " " $0}' \
 	| sort \
 	| cut -d " " -f2- \
 	>.tmp-tasks
+if ! (($DONT_TEST)) ; then
+	find -name test-ci.sh | sort >>.tmp-tasks
+fi
 TASK_COUNT="$(wc -l .tmp-tasks | cut -d " " -f1)"
 echo "Found $TASK_COUNT tasks."
 
@@ -34,7 +39,13 @@ while read script ; do
 		exit $EXITCODE
 	)
 
-	[ -e "$ENVVAR_STORE" ] && cat "$ENVVAR_STORE" && source "$ENVVAR_STORE" && rm "$ENVVAR_STORE"
+	[ -e "$ENVVAR_STORE" ] \
+		&& cat "$ENVVAR_STORE" \
+		&& cat "$ENVVAR_STORE" >>"$ENVVAR_STORE_ALL" \
+		&& source "$ENVVAR_STORE" \
+		&& rm "$ENVVAR_STORE"
 done <.tmp-tasks
 
+echo
+echo
 echo "all done"
