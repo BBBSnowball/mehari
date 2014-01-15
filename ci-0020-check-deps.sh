e02a93ce48d7cb52a48edc5b64af07865132d42f
Benjamin Koch <bbbsnowball@gmail.com>
2014-01-15 03:10:09 +0100

    add information about headers and libraries that we need

--- ci-0020-check-deps.sh
+++ ci-0020-check-deps.sh
@@ -113,3 +113,12 @@ fi
 echo -n "I couldn't find any required tool or library that is missing. However, this script doesn't check "
 echo    "every dependency, so something could be missing. Please report any omission that you find."
 echo
+
+echo -n "In particular, I haven't checked for any headers and libraries that we need. Most build scripts will "
+echo -n "do that themselves. Those files are usually in a package called something-dev or something-devel. You "
+echo    "will need headers and libraries for:"
+echo    "- Python (python"
+echo    "- ctemplate (python2.7-dev)"
+echo
+# Furthermore, the build scripts are much more thorough than a simple check in this could be, so we would
+# have lot's of false positives.
