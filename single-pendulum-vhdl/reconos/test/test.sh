9b086e1f7d9513486cd27c081b04057ba682299d
Benjamin Koch <bbbsnowball@gmail.com>
2014-02-19 06:14:22 +0100

    move (or rather symlink) files into hwt_single_pendulum_vhdl_v1_00_c library

--- single-pendulum-vhdl/reconos/test/test.sh
+++ single-pendulum-vhdl/reconos/test/test.sh
@@ -10,19 +10,13 @@ cd "$(dirname "$0")"
 
 check_environment
 
+../hw/hwt_single_pendulum_vhdl_v1_00_c/create_pao_file.sh
+
 createProjectFile all.prj \
 	"$RECONOS/pcores/reconos_v3_01_a/data/reconos_v2_1_0.pao" \
 	"$RECONOS/pcores/reconos_test_v3_01_a/data/reconos_test_v2_1_0.pao" \
 	"$XILINX/../EDK/hw/XilinxProcessorIPLib/pcores/proc_common_v3_00_a/data/proc_common_v2_1_0.pao" \
 	"../hw/hwt_single_pendulum_vhdl_v1_00_c/data/hwt_single_pendulum_vhdl_v2_1_0.pao" \
-	library:hwt_single_pendulum_vhdl_v1_00_c \
-	"../../double_type.vhd" \
-	"../../float_helpers.vhd" \
-	"../../float_pkg_c_min.vhd" \
-	"../../single_pendulum.vhd" \
-	../../float_{neg,sin,cos}.vhd \
-	../../ipcore_dir/*.vhd \
-	library:work \
 	"single_pendulum_simple_test.vhd"
 
 runTest all.prj work.single_pendulum_simple_test || exit $?
