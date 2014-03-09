58588f2375fb29f85691a79e2476289b2e9eb429
Benjamin Koch <bbbsnowball@gmail.com>
2014-03-09 20:27:08 +0100

    make gtestIncludePath and gtestLibPath file objects (instead of strings)

--- buildSrc/src/main/groovy/helpers.groovy
+++ buildSrc/src/main/groovy/helpers.groovy
@@ -10,14 +10,14 @@ class HelpersPluginConvention {
 
 	public final String pythonInstallPath
 
-	public final String gtestIncludePath, gtestLibPath
+	public final File gtestIncludeDir, gtestLibDir
 
 	public HelpersPluginConvention(Project project) {
 		this.project = project
 		this.pythonInstallPath = project.file(project.rootDir.toString() + "/tools/_install").absolutePath
 
-		this.gtestIncludePath = project.file(project.rootDir.toString() + "/tools/_install/include").absolutePath
-		this.gtestLibPath     = project.file(project.rootDir.toString() + "/tools/_install/lib"    ).absolutePath
+		this.gtestIncludeDir = project.file(project.rootDir.toString() + "/tools/_install/include")
+		this.gtestLibDir     = project.file(project.rootDir.toString() + "/tools/_install/lib"    )
 
 		this.addExtensions()
 	}
