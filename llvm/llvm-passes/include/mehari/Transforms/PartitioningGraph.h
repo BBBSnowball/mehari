894ab305b90bdba94e277008b24b90439e6d3021
Sebastian Surmund <ssurmund@gmail.com>
2014-03-09 14:31:29 +0100

    implement partitioning graph: construction, console and graphviz output

--- llvm/llvm-passes/include/mehari/Transforms/PartitioningGraph.h
+++ llvm/llvm-passes/include/mehari/Transforms/PartitioningGraph.h
@@ -1,40 +1,65 @@
+#ifndef PARTITIONING_GRAPH_H
+#define PARTITIONING_GRAPH_H
+
 #include "llvm/IR/Function.h"
 
+#include "mehari/Analysis/InstructionDependencyAnalysis.h"
+
 #include <boost/graph/adjacency_list.hpp> 
+#include <boost/graph/graphviz.hpp>
 
 #include <vector>
+
 
 using namespace llvm;
 
 class PartitioningGraph {
 
 public:
+
 	PartitioningGraph();
 	~PartitioningGraph();
 
-	void buildGraph(Function &func);
-	void printGraph(void);
-
-private:
-	void createVertices(Function &func);
-	void addEdges();
 
 	struct ComputationUnit {
 		std::string name;
+		unsigned int partition;
 		std::vector<Instruction*> instructions;
 	};
 
 	struct Communication {
-		float cost;
+		unsigned int cost;
 	};
 
 	typedef boost::adjacency_list<
-		boost::listS, // store out-edges of each vertex in a std::list
-		boost::listS, // store vertex set in a std::list
-		boost::directedS, // the graph is directed to represend dependencies
-		ComputationUnit,
-		Communication
+		boost::setS, 				// store out-edges of each vertex in a set to avoid parallel edges
+		boost::listS, 			// store vertex set in a std::list
+		boost::directedS, 	// the graph has got directed edges
+		ComputationUnit,		// the vertices are of type ComputationUnit
+		Communication				// the edges are of type Communication
 		> Graph;
 
+	typedef Graph::vertex_iterator VertexIterator;
+	typedef Graph::vertex_descriptor VertexDescriptor;
+
+	VertexIterator getFirstIterator();
+	VertexIterator getEndIterator(); 
+
+	void setPartition(VertexDescriptor vd, unsigned int partition);
+
+	void createVertices(std::vector<Instruction*> &instructions);
+	void addEdges(InstructionDependencyList &dependencies);
+
+	void printGraph(void);
+	void printGraphviz(void);
+
+private:
+
+	Graph::vertex_descriptor getVertexForInstruction(Instruction *instruction);
+	void addInstructionsToList(std::vector<Instruction*> instructions);
+
+	std::vector<Instruction*> instructionList;
 	Graph pGraph;
 };
+
+#endif /*PARTITIONING_GRAPH_H*/
--- llvm/llvm-passes/lib/Transforms/PartitioningGraph.cpp
+++ llvm/llvm-passes/lib/Transforms/PartitioningGraph.cpp
@@ -1,7 +1,6 @@
 #include "mehari/Transforms/PartitioningGraph.h"
 
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/InstIterator.h"
 
 #include <sstream>
 
@@ -10,31 +9,9 @@ PartitioningGraph::PartitioningGraph() {}
 PartitioningGraph::~PartitioningGraph() {}
 
 
-void PartitioningGraph::buildGraph(Function &func) {
-	createVertices(func);
-	addEdges();
-}
-
-
-void PartitioningGraph::printGraph(void) {
-
-	Graph::vertex_iterator vertexIt, vertexEnd;
-	for (boost::tie(vertexIt, vertexEnd) = boost::vertices(pGraph); vertexIt != vertexEnd; ++vertexIt) { 
-		errs() << "\n";
-		errs() << "instructions in vertex: " << pGraph[*vertexIt].name << "\n";
-		for (std::vector<Instruction*>::iterator instrIt = pGraph[*vertexIt].instructions.begin(); instrIt != pGraph[*vertexIt].instructions.end(); ++instrIt)
-			errs() << "   " << *dyn_cast<Instruction>(*instrIt) << "\n";
-	}
-}
-
-
-void PartitioningGraph::createVertices(Function &func) {
-	// create vertices from instructions inside a function by cutting 
+void PartitioningGraph::createVertices(std::vector<Instruction*> &instructions) {
+	// create vertices from instructions by cutting 
 	// the instruction list at store instructions
-
-	std::vector<Instruction*> worklist;
-	for (inst_iterator I = inst_begin(func), E = inst_end(func); I != E; ++I)
-		worklist.push_back(&*I);
 
 	std::string allocaOpcode = "alloca";
 	std::string storeOpcode = "store";
@@ -44,13 +21,15 @@ void PartitioningGraph::createVertices(Function &func) {
 	std::vector<Instruction*> currentInstrutions;
 	Graph::vertex_descriptor initVertex = boost::add_vertex(pGraph);
 	pGraph[initVertex].name = "init";
-	for (std::vector<Instruction*>::iterator instrIt = worklist.begin(); instrIt != worklist.end(); ++instrIt) {
+	for (std::vector<Instruction*>::iterator instrIt = instructions.begin(); instrIt != instructions.end(); ++instrIt) {
 		Instruction *instr = dyn_cast<Instruction>(*instrIt);
 		currentInstrutions.push_back(instr);
 		if (init) {
 			std::string opcode = dyn_cast<Instruction>(*(instrIt+1))->getOpcodeName();
 			if (opcode.compare(allocaOpcode) != 0 && opcode.compare(storeOpcode) != 0) {
 				pGraph[initVertex].instructions = currentInstrutions;
+				// save the instruction list the graph is based on
+				addInstructionsToList(currentInstrutions);
 				currentInstrutions.clear();
 				init = false;
 			}
@@ -63,6 +42,8 @@ void PartitioningGraph::createVertices(Function &func) {
 				ss << vertexNumber++;
 				pGraph[newVertex].name = ss.str();
 				pGraph[newVertex].instructions = currentInstrutions;
+				// save the instruction list the graph is based on
+				addInstructionsToList(currentInstrutions);
 				currentInstrutions.clear();
 			} 
 		}
@@ -70,7 +51,118 @@ void PartitioningGraph::createVertices(Function &func) {
 }
 
 
-void PartitioningGraph::addEdges() {
-	// add edges between the vertices of the partitioning graph
+void PartitioningGraph::addEdges(InstructionDependencyList &dependencies) {
+	// add edges between the vertices (ComputationUnit) of the partitioning graph
 	// that represent dependencies between the instructions inside the vertices
+	int index = 0;
+	for (std::vector<Instruction*>::iterator instrIt = instructionList.begin(); instrIt != instructionList.end(); ++instrIt, ++index) {
+		// find the ComputationUnit that contains the target instruction
+		Graph::vertex_descriptor targetVertex = getVertexForInstruction(*instrIt);
+		for (InstructionDependencies::iterator depIt = dependencies[index].begin(); depIt != dependencies[index].end(); ++depIt) {
+			// find the ComputationUnit that contains the dependency 
+			// if this ComputationUnit is not the one the target instruction is located in, create an edge between the two ComputationUnits
+			Graph::vertex_descriptor dependencyVertex = getVertexForInstruction(instructionList[*depIt]);
+			if (targetVertex != dependencyVertex) {
+				Graph::edge_descriptor ed;
+				bool inserted;
+				boost::tie(ed, inserted) = boost::add_edge(dependencyVertex, targetVertex, pGraph);
+				if (inserted)
+					// initialize the communication cost
+					pGraph[ed].cost = 1;
+				else
+					// there already is an edge between the two vertices -> increment the communication cost
+					pGraph[ed].cost += 1;
+			}	
+		}
+	}
+}
+
+
+PartitioningGraph::VertexDescriptor PartitioningGraph::getVertexForInstruction(Instruction *instruction) {
+	Graph::vertex_iterator vertexIt, vertexEnd;
+	for (boost::tie(vertexIt, vertexEnd) = boost::vertices(pGraph); vertexIt != vertexEnd; ++vertexIt) {
+		std::vector<Instruction*> instrList = pGraph[*vertexIt].instructions;
+		if (std::find(instrList.begin(), instrList.end(), instruction) != instrList.end())
+			return *vertexIt;
+	}
+}
+
+
+void PartitioningGraph::addInstructionsToList(std::vector<Instruction*> instructions) {
+	for (std::vector<Instruction*>::iterator it = instructions.begin(); it != instructions.end(); ++it)
+		instructionList.push_back(*it);
+}
+
+
+PartitioningGraph::VertexIterator PartitioningGraph::getFirstIterator() {
+	VertexIterator vIt, vEnd;
+	boost::tie(vIt, vEnd) = boost::vertices(pGraph);
+	return vIt;
+}
+
+PartitioningGraph::VertexIterator PartitioningGraph::getEndIterator() {
+	VertexIterator vIt, vEnd;
+	boost::tie(vIt, vEnd) = boost::vertices(pGraph);
+	return vEnd;
+}
+
+
+void PartitioningGraph::setPartition(PartitioningGraph::VertexDescriptor vd, unsigned int partition) {
+	pGraph[vd].partition = partition;	
+}
+
+
+void PartitioningGraph::printGraph(void) {
+	errs() << "\nVERTICES:\n";
+	Graph::vertex_iterator vertexIt, vertexEnd;
+	for (boost::tie(vertexIt, vertexEnd) = boost::vertices(pGraph); vertexIt != vertexEnd; ++vertexIt) { 
+		errs() << "\n";
+		errs() << "vertex " << pGraph[*vertexIt].name << ":\n";
+		errs() << "-------\n";
+		errs() << "partition: " << pGraph[*vertexIt].partition << "\n";
+		errs() << "instructions in vertex:\n";
+		for (std::vector<Instruction*>::iterator instrIt = pGraph[*vertexIt].instructions.begin(); instrIt != pGraph[*vertexIt].instructions.end(); ++instrIt)
+			errs() << "   " << *dyn_cast<Instruction>(*instrIt) << "\n";
+	}
+
+	errs() << "\nEDGES:\n";
+	Graph::edge_iterator edgeIt, edgeEnd;
+	for (boost::tie(edgeIt, edgeEnd) = boost::edges(pGraph); edgeIt != edgeEnd; ++edgeIt) {
+    Graph::vertex_descriptor u = boost::source(*edgeIt, pGraph), v = boost::target(*edgeIt, pGraph);
+		errs() << pGraph[u].name << " -> " << pGraph[v].name << " (cost: " << pGraph[*edgeIt].cost << ")\n";
+	}
+}
+
+
+void PartitioningGraph::printGraphviz(void) {
+	// set some node colors to visualize the different partitions
+	const char *nodeColors[] = { "greenyellow", "gold", "cornflowerblue", "darkorange", "aquamarine1" };
+
+  std::ofstream dotfile("_output/partitioning-graph.dot");
+
+  dotfile << "digraph G {\n"
+    << "  edge[style=\"bold\"];\n" 
+    << "  node[shape=\"rectangle\"];\n";
+
+  Graph::vertex_iterator vertexIt, vertexEnd;
+	for (boost::tie(vertexIt, vertexEnd) = boost::vertices(pGraph); vertexIt != vertexEnd; ++vertexIt) {
+		dotfile << "  " << pGraph[*vertexIt].name << "[style=filled, fillcolor=" << nodeColors[pGraph[*vertexIt].partition] << ", label=\"" << pGraph[*vertexIt].name << ":\\n\\n\"\n";
+		std::vector<Instruction*> instructions = pGraph[*vertexIt].instructions;
+		for (std::vector<Instruction*>::iterator instrIt = instructions.begin(); instrIt != instructions.end(); ++instrIt) {
+			std::string instrString;
+			Instruction *instr = dyn_cast<Instruction>(*instrIt);
+    	llvm::raw_string_ostream rso(instrString);
+    	instr->print(rso);
+    	dotfile << "  + \"" << instrString << "\\l\"\n";
+  	}
+		dotfile << "  ];\n";
+	}
+
+	Graph::edge_iterator edgeIt, edgeEnd;
+	for (boost::tie(edgeIt, edgeEnd) = boost::edges(pGraph); edgeIt != edgeEnd; ++edgeIt) {
+    Graph::vertex_descriptor u = boost::source(*edgeIt, pGraph), v = boost::target(*edgeIt, pGraph);
+		dotfile << "  " << pGraph[u].name << "->" << pGraph[v].name << "[label=\"" << pGraph[*edgeIt].cost << "\"];\n";
+	}
+
+  dotfile << "}";
 }
