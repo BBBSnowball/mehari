1984bd46779076b67a34fdd968e061a923459250
Sebastian Surmund <ssurmund@gmail.com>
2014-03-09 14:33:06 +0100

    implement partitioning pass: create graph, apply random (but deterministic) partitioning, print output (console and graphviz)

--- llvm/llvm-passes/include/mehari/Transforms/PartitioningPass.h
+++ llvm/llvm-passes/include/mehari/Transforms/PartitioningPass.h
@@ -1,5 +1,10 @@
+#ifndef PARTITIONING_PASS_H
+#define PARTITIONING_PASS_H
+
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+
+#include "mehari/Transforms/PartitioningGraph.h"
 
 using namespace llvm;
 
@@ -13,4 +18,12 @@ public:
 
   virtual bool runOnFunction(Function &func);
   virtual void getAnalysisUsage(AnalysisUsage &AU) const;
+
+private:
+	enum Partitions {CPU, FPGA};
+	unsigned int partitionCount = 2;
+
+	void applyRandomPartitioning(PartitioningGraph &pGraph, unsigned int seed);
 };
+
+#endif /*PARTITIONING_PASS_H*/
--- llvm/llvm-passes/lib/Transforms/PartitioningPass.cpp
+++ llvm/llvm-passes/lib/Transforms/PartitioningPass.cpp
@@ -1,5 +1,6 @@
 #include "mehari/Transforms/PartitioningPass.h"
-#include "mehari/Transforms/PartitioningGraph.h"
+
+#include "mehari/Analysis/InstructionDependencyAnalysis.h"
 
 #include "llvm/Support/InstIterator.h"
 #include "llvm/Support/raw_ostream.h"
@@ -10,16 +11,45 @@ PartitioningPass::~PartitioningPass() {}
 
 
 bool PartitioningPass::runOnFunction(Function &func) {
+	// create worklist containing all instructions of the function
+	std::vector<Instruction*> worklist;
+  	for (inst_iterator I = inst_begin(func), E = inst_end(func); I != E; ++I)
+    	worklist.push_back(&*I);
+
+  // run InstructionDependencyAnalysis
+  InstructionDependencyAnalysis *IDA = &getAnalysis<InstructionDependencyAnalysis>();
+  InstructionDependencyList dependencies = IDA->getDependencies(worklist);
+
+  // create partitioning graph
 	PartitioningGraph pGraph;
-	pGraph.buildGraph(func);
+	pGraph.createVertices(worklist);
+	pGraph.addEdges(dependencies);
+
+	// create partitioning
+	applyRandomPartitioning(pGraph, 42);
+
+	// print results
 	pGraph.printGraph();
+	pGraph.printGraphviz();
 
 	return false;
 }
 
 
 void PartitioningPass::getAnalysisUsage(AnalysisUsage &AU) const {
+  AU.addRequiredTransitive<InstructionDependencyAnalysis>();
   AU.setPreservesAll();
+}
+
+
+void PartitioningPass::applyRandomPartitioning(PartitioningGraph &pGraph, unsigned int seed) {
+	srand(seed);
+	PartitioningGraph::VertexIterator vIt = pGraph.getFirstIterator(); 
+	PartitioningGraph::VertexIterator vEnd = pGraph.getEndIterator();
+	for (; vIt != vEnd; ++vIt) {
+		PartitioningGraph::VertexDescriptor vd = *vIt;
+		pGraph.setPartition(vd, rand()%partitionCount);
+	}
 }
 
 
