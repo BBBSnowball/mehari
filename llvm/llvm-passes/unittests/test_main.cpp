44cf9c52986b0b2063f26ece02143101101221bd
Sebastian Surmund <ssurmund@gmail.com>
2014-03-28 14:19:30 +0100

    use gtest from tools instead of the gtest comming with llvm; so we are independent from a certain llvm version and also have tab support, which is not included in the llvm gtest

--- llvm/llvm-passes/unittests/test_main.cpp
+++ llvm/llvm-passes/unittests/test_main.cpp
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
 #include "gtest/gtest.h"
+#include "gtest/tap.h"
+
+void enableTap() {
+// see https://github.com/kinow/gtest-tap-listener/blob/master/samples/src/gtest_main.cc
+testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
+listeners.Append(new tap::TapListener());
+}
 
 GTEST_API_ int main(int argc, char **argv) {
-  printf("Running main() from gtest_main.cc\n");
+	// ::testing::InitGoogleMock(&argc, argv);
   testing::InitGoogleTest(&argc, argv);
+  enableTap();
   return RUN_ALL_TESTS();
 }
