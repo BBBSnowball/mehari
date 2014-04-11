42706ab17befcb54720ce6c66f890aa5f8e787d8
Benjamin Koch <bbbsnowball@gmail.com>
2014-04-12 00:47:37 +0200

    draw plots for single_pendulum_results

--- single-pendulum-vhdl/reconos/evaluate-measurement.py
+++ single-pendulum-vhdl/reconos/evaluate-measurement.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-import re, glob, math
+import re, glob, math, os
+try:
+    import matplotlib.pyplot as pyplot
+    matplotlib_available = True
+except ImportError:
+    matplotlib_available = False
 
 def find_in_file(pattern, filename):
     for line in open(filename, 'r'):
@@ -9,7 +14,7 @@ def find_in_file(pattern, filename):
             yield m.group(1)
 
 def get_command(result_filename):
-    command_line = [x for x in find_in_file(r"^Command: (.*)$", result_filename)]
+    command_line = list(find_in_file(r"^Command: (.*)$", result_filename))
     if len(command_line) >= 1:
         return command_line[0]
     else:
@@ -37,17 +42,67 @@ def get_results():
         command = get_command(filename)
 
         values = find_in_file(r"^\s*Calculation\s*:\s*([0-9]+)\s*ms$", filename)
-        values = [float(value) for value in values]
+        values = map(float, values)
 
         avg = calc_avg(values)
         stdev = calc_stdev(values, avg)
 
-        yield (filename, command, avg, stdev)
+        yield (filename, command, avg, stdev, min(values), max(values), values)
 
 def tsv(*cells):
     return "\t".join(map(str, cells)) + "\n"
 
-with open("results.perf.txt", "w") as f:
-    f.write(tsv("filename", "command", "avg", "stdev"))
-    for result in get_results():
-        f.write(tsv(*result))
+def write_results_file():
+    with open("results.perf.txt", "w") as f:
+        f.write(tsv("filename", "command", "avg", "stdev", "min", "max"))
+        for result in get_results():
+            f.write(tsv(*result[0:-1]))
+
+def frange(start, end, count):
+    if count == 0:
+        return []
+    elif count == 1:
+        return [start]
+
+    xs = [None] * count
+    for i in range(count):
+        xs[i] = start + (end-start)/(count-1)*i
+
+    return xs
+
+#def plot_results():
+#    matplotlib doesn't work at all on my system... :-(
+#    x = []
+#    y = []
+#    for filename, command, avg, stdev, min, max, values in get_results():
+#        n = len(values)
+#        x.append(frange(0.1, 0.5, n))
+#        y.append(values)
+#    pyplot.scatter(x, y)
+#    pyplot.show()
+
+def gnuplot_results():
+    xs = [-0.2]
+    ys = [0]
+    with open("avg.dat", "w") as f:
+        i = 0
+        for filename, command, avg, stdev, min, max, values in get_results():
+            n = len(values)
+            xs.extend(frange(i + 0.1, i + 0.5, n))
+            ys.extend(values)
+
+            f.write(tsv(i, avg, stdev, min, max, "\"" + command.replace(" ", "\\n") + "\""))
+
+            i += 1
+
+    with open("data.dat", "w") as f:
+        for x, y in zip(xs, ys):
+            f.write("%f\t%f\n" % (x, y))
+
+    os.system("gnuplot-nox result.gplot")
+
+write_results_file()
+#if matplotlib_available or True:
+#    plot_results()
+if os.system("which gnuplot-nox >/dev/null") == 0:
+    gnuplot_results()
