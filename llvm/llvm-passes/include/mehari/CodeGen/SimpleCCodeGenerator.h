e8c0c5b11946cbfb41c6e976cc6613c36a5284f4
Sebastian Surmund <ssurmund@gmail.com>
2014-04-16 17:31:56 +0200

    restructure C code generator

--- llvm/llvm-passes/include/mehari/CodeGen/SimpleCCodeGenerator.h
+++ llvm/llvm-passes/include/mehari/CodeGen/SimpleCCodeGenerator.h
@@ -21,15 +21,42 @@ public:
 
 private:
   std::map<Value*, std::string> variables;
-  std::map<Value*, std::string> phiNodeOperands;
   std::map<Value*, std::vector<std::string> > branchLabels;
+  std::map<std::string, std::vector<std::string> > tmpVariables;
+  unsigned int tmpVarNumber;
+  unsigned int tmpLabelNumber;
   
   std::map<std::string, std::string> binaryOperatorStrings;
   std::map<unsigned int, std::string> comparePredicateStrings;  
 
+  void resetVariables();
+
+  void addVariable(Value *addr, std::string name);
+  void addVariable(Value *addr, std::string name, std::string index);
+  void copyVariable(Value *source, Value *target);
+  void addIndexToVariable(Value *source, Value *target, std::string index);
+
   std::string parseBinaryOperator(std::string opcode);
   std::string parseComparePredicate(unsigned int predicateNumber);
+
   std::string getOperandString(Value* addr);
+
+  std::string createTemporaryVariable(Value *addr, std::string datatype);
+  std::string getTemporaryVariable(Value *addr);
+
+  std::string createBranchLabel(Value *target);
+
+  std::string printStore(Value *op1, Value *op2);
+  std::string printBinaryOperator(std::string tmpVar, Value *op1, Value *op2, std::string opcode);
+  std::string printCall(std::string tmpVar, Value *arg, std::string funcName);
+  std::string printComparison(std::string tmpVar, Value *op1, Value *op2, unsigned int comparePredicate);
+  std::string printIntegerExtension(std::string tmpVar, Value *op);
+  std::string printPhiNodeAssignment(std::string tmpVar, Value *op);
+  std::string printUnconditionalBranch(std::string label);
+  std::string printConditionalBranch(Value *condition, std::string label1, std::string label2);
+  std::string printReturn(Value *retVal);
+
+  std::string printVariableDeclarations();
 };
 
 #endif /*SIMPLE_CCODE_GENERATOR_H*/
--- llvm/llvm-passes/lib/CodeGen/SimpleCCodeGenerator.cpp
+++ llvm/llvm-passes/lib/CodeGen/SimpleCCodeGenerator.cpp
@@ -14,6 +14,7 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 using namespace llvm;
 
@@ -39,17 +40,19 @@ SimpleCCodeGenerator::~SimpleCCodeGenerator() {}
 
 bool SimpleCCodeGenerator::runOnFunction(Function &func) {
 
-  // TODO: add parameter to define the IR code that will be transformed to C code
   if (func.getName() != "evalS")
     return false;
+
+  errs() << "\ngenerate C code for: " << func.getName() << "\n";
 
   std::vector<Instruction*> worklist;
   for (inst_iterator I = inst_begin(func), E = inst_end(func); I != E; ++I)
     worklist.push_back(&*I);
 
-  errs() << "\n\ngenerate C code for: " << func.getName() << "\n";
-  errs() << "--------------------\n";
-  errs() << createCCode(worklist);
+  std::string fileName = "_output/code-generation-" + func.getName().str() + ".c";
+  
+  std::ofstream outputFile(fileName.c_str());
+  outputFile << createCCode(worklist);
 
   return false;
 }
@@ -63,11 +66,10 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
   enum CalcState {LOAD_VAR, GET_INDEX, LOAD_INDEX, APPLY};
   CalcState cstate = LOAD_VAR;
 
-  unsigned int tmpVarNumber = 0;
-  unsigned int tmpLabelNumber = 0;
+  // reset all variables before starting the code generation
+  resetVariables();
 
   std::string ccode;
-  std::map<std::string, std::vector<std::string> > tmpVariables;
 
   for (std::vector<Instruction*>::iterator instrIt = instructions.begin(); instrIt != instructions.end(); ++instrIt) {
 
@@ -77,9 +79,6 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
       prevInstr = dyn_cast<Instruction>(*(instrIt-1));
     if (instrIt+1 != instructions.end())
       nextInstr = dyn_cast<Instruction>(*(instrIt+1));
-
-    // DEBUG
-    // errs() << *instr << " ( " << instr << " )\n";
 
     switch (pstate) {
 
@@ -95,7 +94,7 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
         // TODO: very quick and dirty -> find a better solution for this!
         if (paramName == "status")
           paramName = "*status";
-        variables[instr->getOperand(1)] = paramName;
+        addVariable(instr->getOperand(1), paramName);
         if (!isa<StoreInst>(nextInstr)) {
           pstate = CALC;
         }
@@ -120,17 +119,18 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
 
             // handle load of global variable
             if (GEPOperator *op = dyn_cast<GEPOperator>(dyn_cast<LoadInst>(instr)->getOperand(0))) {
+              std::string name = op->getPointerOperand()->getName();
               std::string index;
               if (op->hasIndices())
                 // NOTE: the last index is the one we want to know
                 for (User::op_iterator it = op->idx_begin(); it != op->idx_end(); ++it)
                   index = dyn_cast<ConstantInt>(*it)->getValue().toString(10, true);
-              std::string name = op->getPointerOperand()->getName();
-              index.empty() ? variables[instr] = name : variables[instr] = name + "[" + index + "]";
+              addVariable(instr, name, index);
             }
             // handle load of local variable
             else {
-              variables[instr] = variables[instr->getOperand(0)];
+              // copy name of loaded value to new variable (with current instruction address)
+              copyVariable(instr->getOperand(0), instr);
             }
 
             if (isa<GetElementPtrInst>(nextInstr))
@@ -149,7 +149,7 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
               errs() << "ERROR: Expected getelementptr instruction but there an instruction of a different type!\n";
 
             std::string index = dyn_cast<llvm::ConstantInt>(instr->getOperand(1))->getValue().toString(10, true);
-            variables[instr] = variables[instr->getOperand(0)] + "[" + index + "]";
+            addIndexToVariable(instr->getOperand(0), instr, index);
             
             if (isa<LoadInst>(nextInstr))
               cstate = LOAD_INDEX;
@@ -164,7 +164,8 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
             if (!isa<LoadInst>(instr))
               errs() << "ERROR: Expected load instruction but there an instruction of a different type!\n";
 
-            variables[instr] = variables[instr->getOperand(0)];
+            // copy name of loaded value to new variable (with current instruction address)
+            copyVariable(instr->getOperand(0), instr);
 
             if (isa<LoadInst>(nextInstr))
               cstate = LOAD_VAR;
@@ -176,114 +177,57 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
 
           case APPLY:
           {
-            // DEBUG
-            // for (User::op_iterator it = instr->op_begin(); it != instr->op_end(); ++it) {
-            //   errs() << "   - " << getOperandString(*it) << "\n";
-            // }
 
             if (isa<StoreInst>(instr)) {
-              ccode += "\t"
-                    +  getOperandString(instr->getOperand(1))
-                    +  " = "
-                    +  getOperandString(instr->getOperand(0))
-                    +  ";\n";
+              ccode += printStore(instr->getOperand(1), instr->getOperand(0));
             }
             else if (isa<BinaryOperator>(instr)) {
-              std::string varNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpVarNumber) )->str();
-              ccode += "\tt" + varNumber
-                    +  " = "
-                    +  getOperandString(instr->getOperand(0))
-                    +  " " + parseBinaryOperator(instr->getOpcodeName()) + " "
-                    +  getOperandString(instr->getOperand(1))
-                    +  ";\n";
-              variables[instr] = "t" + varNumber;
-              tmpVariables["double"].push_back("t" + varNumber);
-              tmpVarNumber++;
+              std::string tmpVar = createTemporaryVariable(instr, "double");
+              ccode += printBinaryOperator(tmpVar, instr->getOperand(0), instr->getOperand(1), instr->getOpcodeName());
             }
             else if (CallInst *sInstr = dyn_cast<CallInst>(instr)) {
-              std::string varNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpVarNumber) )->str();
+              std::string tmpVar = createTemporaryVariable(instr, "double");
               Function *func = sInstr->getCalledFunction();
-              ccode += "\tt"
-                    + varNumber
-                    +  " = "
-                    +  func->getName().str()
-                    +  "(" + getOperandString(sInstr->getOperand(0)) + ")"
-                    +  ";\n";
-              variables[instr] = "t" + varNumber;
-              tmpVariables["double"].push_back("t" + varNumber);
-              tmpVarNumber++;
+              ccode += printCall(tmpVar, sInstr->getOperand(0), func->getName().str());
+            }
+            else if (CmpInst *cmpInstr = dyn_cast<CmpInst>(instr)) {
+              std::string tmpVar = createTemporaryVariable(instr, "double");
+              ccode += printComparison(tmpVar, cmpInstr->getOperand(0), cmpInstr->getOperand(1), cmpInstr->getPredicate());
+            }
+            else if (ZExtInst *extInstr = dyn_cast<ZExtInst>(instr)) {
+              std::string tmpVar = createTemporaryVariable(instr, "int");
+              ccode +=  printIntegerExtension(tmpVar, extInstr->getOperand(0));
             }
             else if (BranchInst *brInstr = dyn_cast<BranchInst>(instr)) {
               if (brInstr->isUnconditional()) {
                 // handle phi nodes branch targets
                 if (PHINode *phiInstr = dyn_cast<PHINode>(&brInstr->getSuccessor(0)->front())) {
-                  // create temporary variable for phi node assignment
-                  if (phiNodeOperands.find(phiInstr) == phiNodeOperands.end()) {
+                  // create temporary variable for phi node assignment before the branch instruction
+                  std::string tmpVar = getTemporaryVariable(phiInstr);
+                  if (tmpVar.empty())
                     // this is the first time handling this phi node --> create new variable 
-                    std::string varNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpVarNumber) )->str();
-                    ccode += "\tt"
-                          +  varNumber
-                          +  " = "
-                          +  getOperandString(prevInstr)
-                          +  ";\n";
-                    phiNodeOperands[phiInstr] = "t" + varNumber;
-                    tmpVariables["double"].push_back("t" + varNumber);
-                    tmpVarNumber++;
-                  }
-                  else {
-                    // this is the second time handling this phi node --> reuse the variable
-                    ccode += "\t"
-                          +  phiNodeOperands[phiInstr]
-                          +  " = "
-                          +  getOperandString(prevInstr)
-                          +  ";\n";
-                  }
+                    tmpVar = createTemporaryVariable(phiInstr, "double");
+                  // print assignment for the phi node variable
+                  ccode += printPhiNodeAssignment(tmpVar, prevInstr);
                 }
-                std::string labelNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpLabelNumber) )->str();
-                ccode += "\tgoto label" + labelNumber + ";\n";
-                // save instruction to be labeled 
-                branchLabels[&brInstr->getSuccessor(0)->front()].push_back("label" + labelNumber);
-                tmpLabelNumber++;
+                // add unconditional branch
+                std::string label = createBranchLabel(&brInstr->getSuccessor(0)->front());
+                ccode += printUnconditionalBranch(label);                
               }
               else {
-                std::string labelNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpLabelNumber) )->str();
-                std::string labelNumber2 = static_cast<std::ostringstream*>( &(std::ostringstream() << (tmpLabelNumber+1)) )->str();
-                ccode += "\tif (" + getOperandString(brInstr->getCondition()) + ")" 
-                      +  " goto label" + labelNumber + ";"
-                      +  " else goto label" + (labelNumber2)
-                      + ";\n";
-                // save instruction to be labeled 
-                branchLabels[&brInstr->getSuccessor(0)->front()].push_back("label" + labelNumber);
-                branchLabels[&brInstr->getSuccessor(1)->front()].push_back("label" + labelNumber2);
-                tmpLabelNumber += 2;
+                // create conditional branch
+                std::string label1 = createBranchLabel(&brInstr->getSuccessor(0)->front());
+                std::string label2 = createBranchLabel(&brInstr->getSuccessor(1)->front());
+                ccode += printConditionalBranch(brInstr->getCondition(), label1, label2);
               }
             }
-            else if (CmpInst *cmpInstr = dyn_cast<CmpInst>(instr)) {
-              std::string varNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpVarNumber) )->str();
-              ccode += "\tt" + varNumber
-                    +  " = ("
-                    +  getOperandString(cmpInstr->getOperand(0))
-                    +  " " + parseComparePredicate(cmpInstr->getPredicate()) + " "
-                    +  getOperandString(cmpInstr->getOperand(1))
-                    +  ");\n";
-              variables[instr] = "t" + varNumber;
-              tmpVariables["int"].push_back("t" + varNumber);
-              tmpVarNumber++;
-            }
             else if (PHINode *phiInstr = dyn_cast<PHINode>(instr)) {
-              // here we do not have to print anything because
-              // the variable that should be used was set at the brancht instructions before
+              // here we do not have to print anything, because
+              // the variable that is used was already set at the brancht instructions before
             }
-            else if (ZExtInst *extInstr = dyn_cast<ZExtInst>(instr)) {
-              std::string varNumber = static_cast<std::ostringstream*>( &(std::ostringstream() << tmpVarNumber) )->str();
-              ccode +=  "\tt" + varNumber
-                    +  " = "
-                    +  "(int)"
-                    +  getOperandString(extInstr->getOperand(0))
-                    +  ";\n";
-              variables[instr] = "t" + varNumber;
-              tmpVariables["int"].push_back("t" + varNumber);
-              tmpVarNumber++;
+            else if (ReturnInst *retInstr = dyn_cast<ReturnInst>(instr)) {
+              if (retInstr->getReturnValue() != NULL)
+                ccode += printReturn(retInstr->getReturnValue());
             }
             else
               errs() << "ERROR: unhandled instruction type: " << *instr << "\n";
@@ -300,18 +244,37 @@ std::string SimpleCCodeGenerator::createCCode(std::vector<Instruction*> &instruc
     } // switch pstate     
   } // for instructions
 
-  // create declarations for temporary variables
-  std::string decl;
-  for (std::map<std::string, std::vector<std::string> >::iterator it=tmpVariables.begin(); it!=tmpVariables.end(); ++it) {
-    decl += "\t" + it->first;
-    for (std::vector<std::string>::iterator varIt = it->second.begin(); varIt != it->second.end(); ++varIt) {
-      decl += " " + *varIt + ",";
-    }
-    decl = decl.substr(0, decl.size()-1) + ";\n";
-  }
-  ccode = decl + "\n" + ccode;
+
+  // add declarations for temporary variables at the beginning of the code
+  ccode = printVariableDeclarations() + "\n" + ccode;
 
   return ccode;
+}
+
+
+void SimpleCCodeGenerator::resetVariables() {
+  variables.clear();
+  branchLabels.clear();
+  tmpVariables.clear();
+  tmpVarNumber = 0;
+  tmpLabelNumber = 0;
+}
+
+
+void SimpleCCodeGenerator::addVariable(Value *addr, std::string name) {
+  variables[addr] = name;
+}
+
+void SimpleCCodeGenerator::addVariable(Value *addr, std::string name, std::string index) {
+  index.empty() ? variables[addr] = name : variables[addr] = name + "[" + index + "]";
+}
+
+void SimpleCCodeGenerator::copyVariable(Value *source, Value *target) {
+  variables[target] = variables[source];
+}
+
+void SimpleCCodeGenerator::addIndexToVariable(Value *source, Value *target, std::string index) {
+  variables[target] = variables[source] + "[" + index + "]";
 }
 
 
@@ -334,9 +297,6 @@ std::string SimpleCCodeGenerator::getOperandString(Value* addr) {
   // return operand if it is a variable and already known
   if (variables.find(addr) != variables.end())
     return variables[addr];
-  // return operand if it was a phi node
-  if (phiNodeOperands.find(addr) != phiNodeOperands.end())
-    return phiNodeOperands[addr];
   // handle global value operands
   else if (GEPOperator *op = dyn_cast<GEPOperator>(addr)) {
     std::string index;
@@ -358,12 +318,111 @@ std::string SimpleCCodeGenerator::getOperandString(Value* addr) {
     double value = cf->getValueAPF().convertToDouble();
     return static_cast<std::ostringstream*>( &(std::ostringstream() << value) )->str();
   }
-  // else return the address string itself
+  // else return the address itself as a string
   else {
     std::stringstream ss;
     ss << addr;
     return "## " + ss.str() + " ##";
   }
+}
+
+
+std::string SimpleCCodeGenerator::createTemporaryVariable(Value *addr, std::string datatype) {
+  std::string newTmpVar = "t" + static_cast<std::ostringstream*>( &(std::ostringstream() << tmpVarNumber++) )->str();
+  variables[addr] = newTmpVar;
+  tmpVariables[datatype].push_back(newTmpVar);
+  return newTmpVar;
+}
+
+std::string SimpleCCodeGenerator::getTemporaryVariable(Value *addr) {
+  if (variables.find(addr) != variables.end())
+    return variables[addr];
+  else
+    return "";
+}
+
+
+std::string SimpleCCodeGenerator::createBranchLabel(Value *target) {
+  std::string label = "label" + static_cast<std::ostringstream*>( &(std::ostringstream() << tmpLabelNumber++) )->str();
+  branchLabels[target].push_back(label);
+  return label;
+}
+
+
+std::string SimpleCCodeGenerator::printStore(Value *op1, Value *op2) {
+  return "\t"
+    +  getOperandString(op1)
+    + " = "
+    +  getOperandString(op2)
+    +  ";\n";
+}
+
+std::string SimpleCCodeGenerator::printBinaryOperator(std::string tmpVar, Value *op1, Value *op2, std::string opcode) {
+  return "\t" + tmpVar
+    +  " = "
+    +  getOperandString(op1)
+    +  " " + parseBinaryOperator(opcode) + " "
+    +  getOperandString(op2)
+    +  ";\n";
+}
+
+std::string SimpleCCodeGenerator::printCall(std::string tmpVar, Value *arg, std::string funcName) {
+  return "\t" + tmpVar
+    +  " = "
+    +  funcName
+    +  "(" + getOperandString(arg) + ")"
+    +  ";\n";
+}
+
+std::string SimpleCCodeGenerator::printComparison(std::string tmpVar, Value *op1, Value *op2, unsigned int comparePredicate) {
+  return "\t" + tmpVar
+    +  " = ("
+    +  getOperandString(op1)
+    +  " " + parseComparePredicate(comparePredicate) + " "
+    +  getOperandString(op2)
+    +  ");\n";
+}
+
+std::string SimpleCCodeGenerator::printIntegerExtension(std::string tmpVar, Value *op) {
+  return "\t" + tmpVar
+    +  " = "
+    +  "(int)"
+    +  getOperandString(op)
+    +  ";\n";
+}
+
+std::string SimpleCCodeGenerator::printPhiNodeAssignment(std::string tmpVar, Value *op) {
+  return "\t" + tmpVar
+    +  " = "
+    +  getOperandString(op)
+    +  ";\n";
+}
+
+std::string SimpleCCodeGenerator::printUnconditionalBranch(std::string label) {
+  return "\tgoto " + label + ";\n";
+}
+
+std::string SimpleCCodeGenerator::printConditionalBranch(Value *condition, std::string label1, std::string label2) {
+  return "\tif (" + getOperandString(condition) + ")"
+    +  " goto " + label1 + ";"
+    +  " else goto " + label2
+    + ";\n";
+}
+
+std::string SimpleCCodeGenerator::printReturn(Value *retVal) {
+  return "\treturn" + getOperandString(retVal) + "\n";
+}
+
+
+std::string SimpleCCodeGenerator::printVariableDeclarations() {
+  std::string decl;
+  for (std::map<std::string, std::vector<std::string> >::iterator it=tmpVariables.begin(); it!=tmpVariables.end(); ++it) {
+    decl += "\t" + it->first;
+    for (std::vector<std::string>::iterator varIt = it->second.begin(); varIt != it->second.end(); ++varIt)
+      decl += " " + *varIt + ",";
+    decl = decl.substr(0, decl.size()-1) + ";\n";
+  }
+  return decl;
 }
 
 
