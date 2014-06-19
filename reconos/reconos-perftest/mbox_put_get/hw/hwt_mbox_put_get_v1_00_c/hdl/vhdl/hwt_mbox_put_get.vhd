8c2b33f42ee83f8d46b24fc772c5b57c20125504
Benjamin Koch <bbbsnowball@gmail.com>
2014-06-19 23:18:44 +0200

    mbox_put_get: add support for testing mboxes and semaphores in a loop

--- reconos/reconos-perftest/mbox_put_get/hw/hwt_mbox_put_get_v1_00_c/hdl/vhdl/hwt_mbox_put_get.vhd
+++ reconos/reconos-perftest/mbox_put_get/hw/hwt_mbox_put_get_v1_00_c/hdl/vhdl/hwt_mbox_put_get.vhd
@@ -52,38 +52,26 @@ architecture implementation of hwt_mbox_put_get is
 	signal rst   : std_logic;
 
 	type STATE_TYPE is (
-					STATE_GET,STATE_PUT,STATE_THREAD_EXIT);
-
-	constant C_INPUT_SIZE  : integer := 9;	-- 5 params, 1 input, 2 states, and the current time
-	constant C_OUTPUT_SIZE : integer := 2;	-- 2 state derivatives
-	constant C_LOCAL_RAM_SIZE          : integer := 2 * (C_INPUT_SIZE + C_OUTPUT_SIZE);	-- double takes 8 bytes, i.e. 2 words
-	constant C_LOCAL_RAM_ADDRESS_WIDTH : integer := clog2(C_LOCAL_RAM_SIZE);
-	constant C_LOCAL_RAM_SIZE_IN_BYTES : integer := 4*C_LOCAL_RAM_SIZE;
+					STATE_START, STATE_ACK, STATE_PUT_N, STATE_GET_N,
+					STATE_POST_N, STATE_WAIT_N, STATE_THREAD_EXIT);
 
 	constant MBOX_RECV  : std_logic_vector(31 downto 0) := x"00000000";
 	constant MBOX_SEND  : std_logic_vector(31 downto 0) := x"00000001";
+	constant MBOX_TEST  : std_logic_vector(31 downto 0) := x"00000002";
+	constant SEM_TEST   : std_logic_vector(31 downto 0) := x"00000003";
 
 	signal addr         : std_logic_vector(31 downto 0);
-	signal addr_outputs : std_logic_vector(31 downto 0);
 	signal len          : std_logic_vector(23 downto 0);
 	signal state        : STATE_TYPE;
 	signal i_osif       : i_osif_t;
 	signal o_osif       : o_osif_t;
 	signal i_memif      : i_memif_t;
 	signal o_memif      : o_memif_t;
-	signal i_ram        : i_ram_t;
-	signal o_ram        : o_ram_t;
-
-	signal o_RAMAddr_reconos   : std_logic_vector(0 to C_LOCAL_RAM_ADDRESS_WIDTH-1);
-	signal o_RAMAddr_reconos_2 : std_logic_vector(0 to 31);
-	signal o_RAMData_reconos   : std_logic_vector(0 to 31);
-	signal o_RAMWE_reconos     : std_logic;
-	signal i_RAMData_reconos   : std_logic_vector(0 to 31);
 	
 	signal ignore   : std_logic_vector(31 downto 0);
+	signal ignore2  : std_logic_vector(31 downto 0);
 
-	signal sort_start : std_logic := '0';
-	signal sort_done  : std_logic := '0';
+	signal count    : std_logic_vector(23 downto 0);
 begin
 
 	clk <= HWT_Clk;
@@ -117,34 +105,86 @@ begin
 	);
 		
 	-- os and memory synchronisation state machine
-	reconos_fsm: process (clk,rst,o_osif,o_memif,o_ram) is
+	reconos_fsm: process (clk,rst,o_osif,o_memif) is
 		variable done  : boolean;
 	begin
 		if rst = '1' then
 			osif_reset(o_osif);
 			memif_reset(o_memif);
-			state <= STATE_GET;
+			state <= STATE_START;
 			done  := False;
 			addr <= (others => '0');
 			len <= (others => '0');
 		elsif rising_edge(clk) then
 			case state is
 
-				-- get address via mbox: the data will be copied from this address to the local ram in the next states
-				when STATE_GET =>
+				-- get task via mbox: the data will be copied from this address to the local ram in the next states
+				when STATE_START =>
 					osif_mbox_get(i_osif, o_osif, MBOX_RECV, addr, done);
 					if done then
-						if (addr = X"FFFFFFFF") then
-							state <= STATE_THREAD_EXIT;
+						case addr(31 downto 24) is
+							when X"FF" =>
+								state <= STATE_THREAD_EXIT;
+							when X"00" =>
+								state <= STATE_ACK;
+							when X"01" =>
+								state <= STATE_PUT_N;
+							when X"02" =>
+								state <= STATE_GET_N;
+							when X"03" =>
+								state <= STATE_POST_N;
+							when X"04" =>
+								state <= STATE_WAIT_N;
+							when others =>
+								state <= STATE_START;
+						end case;
+
+						count <= addr(23 downto 0);
+					end if;
+
+				when STATE_ACK =>
+					osif_mbox_put(i_osif, o_osif, MBOX_SEND, addr, ignore, done);
+					if done then state <= STATE_START; end if;
+
+				when STATE_PUT_N =>
+					osif_mbox_put(i_osif, o_osif, MBOX_TEST, X"00" & count, ignore, done);
+					if done then
+						if count /= X"000000" then
+							count <= count - X"000001";
 						else
-							state <= STATE_PUT;
+							state <= STATE_ACK;
 						end if;
 					end if;
-				
-				-- send mbox that signals that the sorting is finished
-				when STATE_PUT =>
-					osif_mbox_put(i_osif, o_osif, MBOX_SEND, addr, ignore, done);
-					if done then state <= STATE_GET; end if;
+
+				when STATE_GET_N =>
+					osif_mbox_get(i_osif, o_osif, MBOX_TEST, ignore, done);
+					if done then
+						if count /= X"000000" then
+							count <= count - X"000001";
+						else
+							state <= STATE_ACK;
+						end if;
+					end if;
+
+				when STATE_POST_N =>
+					osif_sem_post(i_osif, o_osif, SEM_TEST, ignore, done);
+					if done then
+						if count /= X"000000" then
+							count <= count - X"000001";
+						else
+							state <= STATE_ACK;
+						end if;
+					end if;
+
+				when STATE_WAIT_N =>
+					osif_sem_wait(i_osif, o_osif, SEM_TEST, ignore, done);
+					if done then
+						if count /= X"000000" then
+							count <= count - X"000001";
+						else
+							state <= STATE_ACK;
+						end if;
+					end if;
 
 				-- thread exit
 				when STATE_THREAD_EXIT =>
