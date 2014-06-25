0a6941f89e42a969cf9383439493167507c3391b
Benjamin Koch <bbbsnowball@gmail.com>
2014-06-25 14:50:32 +0200

    fix bugs caused by renaming of the prepare script

--- single-pendulum-vhdl/reconos/test/test.sh
+++ single-pendulum-vhdl/reconos/test/test.sh
@@ -10,12 +10,14 @@ cd "$(dirname "$0")"
 
 check_environment
 
-../hw/hwt_single_pendulum_simple_v1_00_c/create_pao_file.sh
+../hw/float_library_v1_00_a/prepare.sh
+../hw/hwt_single_pendulum_simple_v1_00_c/prepare.sh
 
 createProjectFile all.prj \
 	"$RECONOS/pcores/reconos_v3_01_a/data/reconos_v2_1_0.pao" \
 	"$RECONOS/pcores/reconos_test_v3_01_a/data/reconos_test_v2_1_0.pao" \
 	"$XILINX/../EDK/hw/XilinxProcessorIPLib/pcores/proc_common_v3_00_a/data/proc_common_v2_1_0.pao" \
+	"../hw/float_library_v1_00_a/data/float_library_v2_1_0.pao" \
 	"../hw/hwt_single_pendulum_simple_v1_00_c/data/hwt_single_pendulum_simple_v2_1_0.pao" \
 	"single_pendulum_simple_test.vhd"
 
